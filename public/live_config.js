992ff17a022afd6b828a5639df43f84ee6deb02b
loic <loic.szpieg@gmail.com>
2019-02-15 12:25:16 +0100

    Add histogram

--- public/live_config.js
+++ public/live_config.js
@@ -20,29 +20,48 @@ twitch.onContext(function (c) {
 twitch.onAuthorized(function (auth) {
   token = auth.token;
   tuid = auth.userId;
-  displayHistogram()
+  twitch.listen('broadcast', function (target, contentType, content) {
+    log('Received expressions:');
+    averageMood = JSON.parse(content);
+    displayHistogram(averageMood)
+  });
 });
 
 function getHistory() {
   fetch(EBS_ENDPOINT, {
     method: 'GET',
     headers: new Headers({
-       Token: token,
-     }),
+      Token: token,
+    }),
     mode: 'cors',
   }).then(response => response.json())
-  .then(data => {
-    console.log(data) // Prints result from `response.json()` in getRequest
-    data.map(i => i.dateTime)
-    console.log(data) // Prints result from `response.json()` in getRequest
-  })
-  .catch(error => console.error(error));
+    .then(data => {
+      console.log(data) // Prints result from `response.json()` in getRequest
+      data.map(i => i.dateTime)
+      console.log(data) // Prints result from `response.json()` in getRequest
+    })
+    .catch(error => console.error(error));
 }
 
-function displayHistogram(){
+function displayHistogram(averageMood) {
   console.log("Starting displayHistogram")
-  getHistory();
-  console.log("Ending displayHistogram")
+  var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
+  var svg = d3.select("#histogram")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+  var x = d3.scaleLinear()
+    .domain([angry,disgusted,fearful,happy,neutral,sad,surprised])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .range([0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+  console.log("Ending displayHistogram")
 }
 
