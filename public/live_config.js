912e032ef6de7aaf7ca9f7e45dff8053fe70ceeb
loic <loic.szpieg@gmail.com>
2019-02-16 18:02:51 +0100

    chart

--- public/live_config.js
+++ public/live_config.js
@@ -10,6 +10,33 @@ var chartRange;
 var ndx;
 var moodDimension;
 
+function nonzero_min(chart) {
+  dc.override(chart, "yAxisMin", function () {
+      var min = d3.min(chart.data(), function (layer) {
+          return d3.min(layer.values, function (p) {
+              return p.y + p.y0;
+          });
+      });
+      return dc.utils.subtract(min, chart.yAxisPadding());
+  });
+  return chart;
+}
+
+var groups_by_min_interval = [
+  {
+    name: 'minutes',
+    threshold: 60 * 60 * 1000,
+    interval: d3.timeMinute
+  }, {
+    name: 'seconds',
+    threshold: 60 * 1000,
+    interval: d3.timeSecond
+  }, {
+    name: 'milliseconds',
+    threshold: 0,
+    interval: d3.timeMillisecond
+  }
+];
 
 function log(message) {
   if (typeof message === 'string') {
@@ -39,14 +66,69 @@ twitch.onAuthorized(function (auth) {
   });
 });
 
-async function  initCharts() {
+async function initCharts() {
   var history = getHistory('{"operator":">"}');
-  // var chartBar = dc.lineChart("#chartLine");
-  // var chartRange = dc.lineChart("#chartRange");
-  // var fullDomain = [data[0].date, data.slice(-1)[0].date];
-  // var dimension = crossfilter(data).dimension(function (d) {
-  //   return d.date;
-  // });
+  var chartBar = dc.lineChart("#chartLine");
+  var chartRange = dc.lineChart("#chartRange");
+  var fullDomain = [history.items[0].datetime, new Date(t)];
+  var dimension = crossfilter(history.items).dimension(function (d) {
+    return d.datetime;
+  });
+  function make_group(interval) {
+    return dimension.group(interval).reduce(
+      function (p, v) {
+        p.count++;
+        p.total += v.value;
+        return p;
+      },
+      function (p, v) {
+        p.count--;
+        p.total += v.value;
+        return p;
+      },
+      function () {
+        return { count: 0, total: 0 };
+      }
+    );
+  }
+  chart
+    .width(800)
+    .height(300)
+    .dimension(dimension)
+    .group(choose_group(fullDomain))
+    .yAxisPadding(0.1)
+    .valueAccessor(function (kv) { return kv.mood.angry / kv.number; })
+    .rangeChart(rangeChart)
+    .x(d3.scaleTime().domain(fullDomain))
+    .xUnits(d3.timeDay)
+    .brushOn(false)
+    .mouseZoomable(true)
+    .zoomScale([1, 100])
+    .zoomOutRestrict(true)
+    .renderVerticalGridLines(true)
+    .elasticY(true)
+    .transitionDuration(100);
+  nonzero_min(chart);
+  rangeChart
+    .width(800)
+    .height(100)
+    .dimension(dimension)
+    .group(groups_by_min_interval[0].group)
+    .yAxisPadding(1)
+    .valueAccessor(function (kv) { return kv.mood.angry / kv.number; })
+    .x(d3.scaleTime().domain(fullDomain))
+    .xUnits(d3.timeDay);
+  rangeChart.on('filtered.dynamic-interval', function (_, filter) {
+    chart.group(choose_group(filter || fullDomain));
+  });
+  chart.yAxis().tickFormat(function (t) {
+    return t.toFixed(0);
+  });
+  rangeChart.yAxis().tickFormat(function (t) {
+    return "";
+  });
+  rangeChart.yAxis().ticks(0);
+  dc.renderAll();
 }
 
 async function getHistory(params) {
