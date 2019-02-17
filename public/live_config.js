980e044fd34247a65c2b45e889d40adda9052d72
loic <loic.szpieg@gmail.com>
2019-02-17 16:45:37 +0100

    merge

--- public/live_config.js
+++ public/live_config.js
@@ -102,10 +102,10 @@ function initCharts(history) {
     .xUnits(d3.timeDay)
     .centerBar(true)
     .gap(1)
-    .round(d3.timeSecond.round)
-    .alwaysUseRounding(true);
+    // .round(d3.timeSecond.round)
+    // .alwaysUseRounding(true);
   chartRange.on('filtered.dynamic-interval', function (_, filter) {
-    chartBar.group(choose_group(filter || fullDomain));
+    //chartBar.group(choose_group(filter || fullDomain));
   });
   dc.renderAll();
 }
