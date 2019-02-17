add38302c307d0bf8af4bbc7ecdd973f24a069b1
Christophe Bougère <christophe.bougere@gmail.com>
2019-02-17 15:34:31 +0100

    fromDate

--- public/live_config.js
+++ public/live_config.js
@@ -72,8 +72,9 @@ function loadData(){
   log(JSON.stringify(history))
   chartBar = dc.lineChart("#chartLine");
   chartRange = dc.lineChart("#chartRange");
-  log (new Date(history.items[0].datetime));
-  var fullDomain = [new Date(history.items[0].datetime), new Date()];
+  var fromDate = (history.items[0] && new Date(history.items[0].datetime)) || new Date();
+  log(fromDate);
+  var fullDomain = [fromDate, new Date()];
   var dimension = crossfilter(history.items).dimension(function (d) {
     return d.datetime;
   });
