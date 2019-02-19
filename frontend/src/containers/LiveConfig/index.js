d2a6d966c69fc7313fb2311f9c61667556703876
loic <loic.szpieg@gmail.com>
2019-02-19 16:02:36 +0100

    d3 scaletime

--- frontend/src/containers/LiveConfig/index.js
+++ frontend/src/containers/LiveConfig/index.js
@@ -54,6 +54,7 @@ class LiveConfig extends Component {
     return dc.lineChart(this.chartComposite)
       .group(this.dimension.group().reduceSum(d => d.mood[mood] / d.number), mood)
       .colors(color)
+      .renderTitle(true)
       .title(function (d) {
         console.log(`d ${JSON.stringify(d)}`);
         return d;
