84535d21a5c5c21dde19d87d4f1ba9b79c883579
loic <loic.szpieg@gmail.com>
2019-02-19 16:43:28 +0100

    add mouse over

--- frontend/src/containers/LiveConfig/index.js
+++ frontend/src/containers/LiveConfig/index.js
@@ -50,6 +50,10 @@ class LiveConfig extends Component {
     this.updateCharts();
   }
 
+  formatDatetime(dateObj) {
+    return `${dateObj.toISOString().split('.')[0]}Z`;
+  }
+  
   getLineChart(mood, color) {
     return dc.lineChart(this.chartComposite)
       .group(this.dimension.group().reduceSum(d => d.mood[mood] / d.number), mood)
@@ -80,7 +84,7 @@ class LiveConfig extends Component {
       .legend(dc.legend().autoItemWidth(true).horizontal(true))
       .title((d) => {
         console.log(d);
-        const number = data.find( i => i.datetime === d.key).number;
+        const number = data.find( i => i.datetime === formatDatetime(d.key)).number;
         return `Total users: ${number}`;
       })
       .compose([
