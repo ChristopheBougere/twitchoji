fa0218ef9ac92795efaac50dd9d155f9886c7f00
loic <loic.szpieg@gmail.com>
2019-02-21 17:05:58 +0100

    interpolate graph

--- frontend/src/containers/LiveConfig/index.js
+++ frontend/src/containers/LiveConfig/index.js
@@ -83,7 +83,7 @@ class LiveConfig extends Component {
     return dc.lineChart(this.chartComposite)
       .group(this.dimension.group().reduceSum(d => d.mood[mood] / d.number), mood)
       .colors(color)
-      .interpolate(basis);
+      .interpolate('basis');
   }
 
   initCharts(fromDatetime, endDate) {
